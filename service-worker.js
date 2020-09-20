0d02cab97fd102ca1740c7545c2039c56acde52a
Koteswarrao375 <koteswar.meesala.ext@siemens.com>
2020-09-20 18:04:00 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/suvobday/precache-manifest.ff226d9eb54202d5506d1d0308875a9d.js"
+  "/suvobday/precache-manifest.1bc28d4b7c9836105ebee963e5580d28.js"
 );
 
 self.addEventListener('message', (event) => {
