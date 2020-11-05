15310012ed2d651a42580c71b800ab013a6b06f0
Koteswarrao375 <koteswar.meesala.ext@siemens.com>
2020-11-05 09:56:17 +0530

    fixes

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -4,20 +4,23 @@ import ProfilePic from '../../photos/ProfilePic/Profile.jpg';
 
 const AboutMe = (props) => {
 
-  
+
 
     return (
         <div className="aboutme">
-                <img className="profilePic" alt="profilepic" src={ProfilePic} />
+            <img className="profilePic" alt="profilepic" src={ProfilePic} />
+            <div className="aboutme-content">
+                <label>ABOUT ME</label>
                 <section>
-                Based in Kolkata, I am an evolving and upcoming actor/model cum software engineer. I have done a variety of 
-                modelling/acting work from model-oriented photo-shoot to commercial TV serials. 
-                I am an outgoing and modest personality which makes me easy to work with. 
-                I consider myself to be punctual and always on time. I am open for any kind of modelling work and acting.
-                Please feel free to contact me.
+                    Based in Kolkata, I am an evolving and upcoming actor/model cum software engineer. I have done a variety of
+                    modelling/acting work from model-oriented photo-shoot to commercial TV serials.
+                    I am an outgoing and modest personality which makes me easy to work with.
+                    I consider myself to be punctual and always on time. I am open for any kind of modelling work and acting.
+                    Please feel free to contact me.
                 </section>
+            </div>
         </div>
-        
+
     )
 }
 
--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,21 +1,21 @@
 import React from 'react';
-import './Contact.css';
+import './Contact.scss';
 
 const Contact = (props) => {
 
     return (
        <div className="contactForm">
            <div className="row">
-               <label className="col s6">Name</label>
-               <label className="col s6">Suvojit Dhar Chowdhury</label>
+               <label className="col s4 l6">Name</label>
+               <label className="col s8 l6">Suvojit Dhar Chowdhury</label>
            </div>
            <div className="row">
-               <label className="col s6">Email</label>
-               <label className="col s6">suvojitdharchowdhury@gmail.com</label>
+               <label className="col s4 l6">Email</label>
+               <label className="col s8 l6">suvojitdharchowdhury@gmail.com</label>
            </div>
            <div className="row">
-               <label className="col s6">Mobile</label>
-               <label className="col s6">8918203030</label>
+               <label className="col s4 l6">Mobile</label>
+               <label className="col s8 l6">8918203030</label>
            </div>
        </div>
     )
